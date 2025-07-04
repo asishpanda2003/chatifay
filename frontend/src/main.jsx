b33f709f408da4d47541b2c8764eeb3b8d0b156c
Asish kumar Panda <112423732+asishpanda2003@users.noreply.github.com>
2025-07-04 11:52:50 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 
-export const serverUrl = "http://localhost:3000";
+export const serverUrl = "https://chatifay-backend.onrender.com";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
