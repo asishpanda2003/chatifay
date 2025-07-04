799c8a10324d033e10795335c9baa8b71ed9154c
Asish kumar Panda <112423732+asishpanda2003@users.noreply.github.com>
2025-07-04 12:00:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const port=process.env.PORT || 5000;
 
 //middlewares
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://chatifay.onrender.com",
     credentials: true
 }))
 app.use(express.json());
